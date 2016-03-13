0b04d49d84c63b85760b3594b4a4f6e5e756611f
Robert D Herb <github@robertdherb.com>
2016-03-13 16:47:07 -0500

    SDL not built with Haptic support, so don't use SDL_INIT_EVERYTHING

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Learn SDL
 
-This is my attempt to learn that thing called SDL. I'll be primarily using [this tutorial](http://stephenmeier.net/2014/08/10/sdl-2-0-tutorial-00-the-basic-structure) and, of course, the [SDL API docs](https://wiki.libsdl.org/APIByCategory).
+This is my attempt to learn that thing called SDL. I'll be primarily using [this tutorial](http://stephenmeier.net/2014/08/10/sdl-2-0-tutorial-00-the-basic-sturcture/) and, of course, the [SDL API docs](https://wiki.libsdl.org/APIByCategory).
--- main.c
+++ main.c
@@ -1,7 +1,95 @@
+#define SDL_MAIN_HANDLED
+#define SCREEN_w 640
+#define SCREEN_H 480
+#define SCREEN_SCALE 1
+#define SCREEN_NAME "Prototype"
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "SDL.h"
 #include "SDL_video.h"
+
+void game_init( void );
+void game_quit( void );
+
+static struct {
+	/* Define Attributes */
+		SDL_bool running;
+		struct {
+			unsigned int w;
+			unsigned int h;
+			const char *name;
+			SDL_Window *window;
+			SDL_Renderer *renderer;
+		} screen;
+	
+	/* Define Methods */
+		void (*init)(void);
+		void (*quit)(void);
+
+} Game = {
+	SDL_FALSE,
+	{
+		SCREEN_SCALE*SCREEN_w,
+		SCREEN_SCALE*SCREEN_H,
+		SCREEN_NAME,
+		NULL,
+		NULL
+	},
+	game_init,
+	game_quit
+};
+
+
+void
+game_init( void )
+{
+
+	if( SDL_Init( SDL_INIT_TIMER|SDL_INIT_AUDIO|SDL_INIT_VIDEO|SDL_INIT_EVENTS ) != 0 ){
+		fprintf( stdout, "SDL failed to initialize, reason: %s\n", SDL_GetError() );
+		exit( 101 );
+	}
+
+	unsigned int w = Game.screen.w;
+	unsigned int h = Game.screen.h;
+	const char *name = Game.screen.name;
+
+	Game.screen.window = SDL_CreateWindow(
+		name,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		w, h, 0
+	);
+	Game.screen.renderer = SDL_CreateRenderer(
+		Game.screen.window, -1,
+		SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC
+	);
+
+	/*
+	 * Maybe use SDL_CreateWindowAndRenderer() ?
+	 */
+
+	Game.running = SDL_TRUE;
+
+}
+
+void
+game_quit( void )
+{
+
+	SDL_DestroyRenderer( Game.screen.renderer );
+	SDL_DestroyWindow( Game.screen.window );
+
+	Game.screen.window = NULL;
+	Game.screen.renderer = NULL;
+
+	SDL_Quit();
+
+	Game.running = SDL_FALSE;
+
+}
+
 
 int
 main( int argc, char *argv[] )
@@ -9,15 +97,24 @@ main( int argc, char *argv[] )
 
 	SDL_Window * theWindow;
 
-	if( SDL_Init( SDL_INIT_VIDEO ) != 0 ){
-		fprintf( stdout, "SDL failed to initialize, reason: %s\n", SDL_GetError() );
+	Game.init();
 
-		return 101;
+	SDL_Event event;
+
+	while( Game.running ) {
+		while( SDL_PollEvent( &event ) ) {
+			switch( event.type ) {
+				case SDL_QUIT: 
+					Game.running = SDL_FALSE;
+				 	break;
+			}
+		}
+		
+		SDL_RenderClear( Game.screen.renderer );
+		SDL_RenderPresent( Game.screen.renderer );
 	}
-
-	theWindow = SDL_CreateWindow( "Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0 );
 	
-	SDL_Quit();
+	Game.quit();
 
 	return 0;
 
